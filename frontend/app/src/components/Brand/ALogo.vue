f4c3e670f3b338f28d583049510121924bdcdfee
ilya-belik <illia.belichenko@gmail.com>
2022-02-28 13:16:21 +0200

    logo component

---
+++ frontend/app/src/components/Brand/ALogo.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- App Logo -->
+  {{ brandName }}
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+// Get App Name from env (quasar.conf.js)
+const brandName = ref(process.env.BRAND_NAME)
+</script>
