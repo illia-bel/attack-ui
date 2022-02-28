57fd92654de61808aeeb21d246b0a6ebc97ee878
ilya-belik <illia.belichenko@gmail.com>
2022-02-28 13:16:12 +0200

    added nav components

---
+++ frontend/app/src/components/Nav/Drawer/ADrawer.vue
@@ -0,0 +1,58 @@
+<template>
+  <q-drawer
+    v-model="leftDrawerState"
+    show-if-above
+    side="left"
+    :width="280"
+    bordered
+  >
+    <a-nav-list :list="navList" />
+  </q-drawer>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import ANavList from 'src/components/Nav/Drawer/ANavList'
+
+const props = defineProps({
+  modelValue: Boolean,
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const leftDrawerState = computed({
+  get() {
+    return props.modelValue
+  },
+  set(state) {
+    emit('update:modelValue', state)
+  },
+})
+
+/**
+ * @example
+ * {
+    text: 'Nav Link Text',
+    icon: '', // icon name
+    // router link object
+    to: {
+      name: 'PageName',
+      // hash, query, path.... 
+    },
+
+    // show if showRule return true
+    showRule: () => {
+      return process.env.dev
+    },
+  },
+ */
+const navList = [
+  {
+    text: 'DDOS Attack',
+    icon: 'dns',
+    to: {
+      name: 'DdosAttack',
+    },
+  },
+]
+</script>
---
+++ frontend/app/src/components/Nav/Drawer/ANavList.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-list separator>
+    <template
+      v-for="({ text, to, showRule, icon }, key) in list"
+    >
+      <q-item
+        v-if="showRule ? showRule() : true"
+        :key="key + text"
+        :to="to"
+        clickable
+        v-ripple
+      >
+        <q-item-section v-if="icon" avatar>
+          <q-icon :name="icon" />
+        </q-item-section>
+        <q-item-section>{{ text }}</q-item-section>
+      </q-item>
+    </template>
+  </q-list>
+</template>
+
+<script setup>
+const props = defineProps({
+  list: {
+    type: Array,
+    default: () => [],
+  },
+})
+const emit = defineEmits([])
+</script>
+
+<style lang="scss" scouped></style>
