bff89bac6de28cf7b6ccf9bfa8b3d5c77d071d75
ilya-belik <illia.belichenko@gmail.com>
2022-02-28 15:53:50 +0200

    top level await

--- frontend/app/quasar.conf.js
+++ frontend/app/quasar.conf.js
@@ -98,6 +98,9 @@ module.exports = configure(function (ctx) {
       chainWebpack(/* chain */) {
         //
       },
+      extendWebpack(cfg) {
+        cfg.experiments = { topLevelAwait: true }
+      },
     },
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
