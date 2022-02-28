39d7e4a2f62cb5a09fa82caf292918fc0275186e
ilya-belik <illia.belichenko@gmail.com>
2022-02-28 13:15:39 +0200

    comment i18n init

--- frontend/app/src/boot/i18n.js
+++ frontend/app/src/boot/i18n.js
@@ -1,13 +1,12 @@
 import { boot } from 'quasar/wrappers'
-import { createI18n } from 'vue-i18n'
-import messages from 'src/i18n'
+// import { createI18n } from 'vue-i18n'
+// import messages from 'src/i18n'
 
 export default boot(({ app }) => {
-  const i18n = createI18n({
-    locale: 'en-US',
-    messages,
-  })
-
+  // const i18n = createI18n({
+  //   locale: 'en-US',
+  //   messages,
+  // })
   // Set i18n instance on app
-  app.use(i18n)
+  // app.use(i18n)
 })
