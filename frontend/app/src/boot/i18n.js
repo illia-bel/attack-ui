c3bbaa58c53c332e69c084ffc6de85db6e1ef54d
ilya-belik <illia.belichenko@gmail.com>
2022-03-01 15:34:56 +0200

    Change Locale

--- frontend/app/src/boot/i18n.js
+++ frontend/app/src/boot/i18n.js
@@ -2,11 +2,16 @@ import { boot } from 'quasar/wrappers'
 import { createI18n } from 'vue-i18n'
 import messages from 'src/i18n'
 
+const userLang =
+  localStorage.getItem('lang') ||
+  navigator.language ||
+  navigator.userLanguage
+
 export let i18n
 export default boot(({ app }) => {
   i18n = createI18n({
     legacy: false,
-    locale: 'en-US',
+    locale: userLang,
     messages,
   })
   // Set i18n instance on app
