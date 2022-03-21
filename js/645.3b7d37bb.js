2261eb6450b6715fd8056349aaf3fc549254a187
ilya-belik <60644182+ilya-belik@users.noreply.github.com>
2022-03-21 18:18:39 +0000

    Build: (e72fb4a) Merge pull request #46 from ilya-belik/dev
    Dev

---
+++ js/645.3b7d37bb.js
@@ -0,0 +1,1 @@
+"use strict";(self["webpackChunkattack_ui_app"]=self["webpackChunkattack_ui_app"]||[]).push([[645],{645:(a,t,u)=>{u.r(t),u.d(t,{default:()=>p});var r=u(536);const s={class:"russian-data-layout row"};function e(a,t){const u=(0,r.up)("router-view");return(0,r.wg)(),(0,r.iD)("div",s,[(0,r.Wm)(u)])}var c=u(9895);const n={},i=(0,c.Z)(n,[["render",e]]),p=i}}]);
