525572f56909827b80a17e08b84096a2c280aa39
NZXTUA <nzxt.ua@gmail.com>
2022-03-23 02:42:26 +0200

    feat: attack status indicator

---
+++ frontend/app/src/components/Nav/Header/AIndicator.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-badge
+    rounded
+    class="bg-deep-orange"
+    :class="isAttackStarted ? 'status-badge ' : 'hidden'"
+  >
+    <q-icon name="bolt" size="14px" />
+  </q-badge>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useStore } from 'vuex';
+
+const store = useStore()
+const isAttackStarted = computed(() => store.getters['ddos/getBrowserAttackStatus'])
+</script>
