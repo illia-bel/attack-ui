f03d1cf71adad3a113525d1c368bcf7e52880e13
ilya-belik <illia.belichenko@gmail.com>
2022-03-24 16:00:26 +0200

    russians data loader

---
+++ frontend/app/src/components/Loaders/ALoaderContainer.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-spinner-ios v-if="loading" color="primary" size="2em" />
+  <slot v-else />
+</template>
+
+<script setup>
+const props = defineProps({
+  loading: Boolean,
+})
+</script>
--- frontend/app/src/pages/RussianData/RussianEmails.vue
+++ frontend/app/src/pages/RussianData/RussianEmails.vue
@@ -10,27 +10,33 @@
 
   <a-card class="col-12 q-mt-md">
     <template #body>
-      <a-data-text-view
-        :data-list="emailsList"
-        source-link="https://raw.githubusercontent.com/NIRVANA101/Russian-GBSB/main/CONTENTS/Emails/emails.json"
-      />
+      <a-loader-container :loading="isDataLoading">
+        <a-data-text-view
+          :data-list="emailsList"
+          source-link="https://raw.githubusercontent.com/NIRVANA101/Russian-GBSB/main/CONTENTS/Emails/emails.json"
+        />
+      </a-loader-container>
     </template>
   </a-card>
 </template>
 
 <script setup>
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { getRussiansEmailsList } from 'src/modules/api'
 
 import ACard from 'src/components/Cards/ACard'
 import ADataTextView from 'src/components/RussianData/ADataTextView'
+import ALoaderContainer from 'src/components/Loaders/ALoaderContainer'
 
 const { t: i18n } = useI18n()
 const emailsList = reactive([])
+const isDataLoading = ref(false)
 
 ;(async () => {
+  isDataLoading.value = true
   emailsList.push(...(await getRussiansEmailsList()))
+  isDataLoading.value = false
 })()
 </script>
 
