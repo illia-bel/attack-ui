14994bd86e594db638a8d2623cd794ebd5f587d7
ilya-belik <illia.belichenko@gmail.com>
2022-03-26 21:51:47 +0200

    fix build.sh

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,5 +1,5 @@
 rm -Rf -d ./frontend/dist && \
 mkdir ./frontend/dist && \
 mkdir ./frontend/dist/site && \
-cp -R ./frontend/app/dist/spa/{*,.*} ./frontend/dist && \
-cp -R ./frontend/site/public/{*,.*} ./frontend/dist/site
+cp -R frontend/app/dist/spa/{*,.*} frontend/dist && \
+cp -R frontend/site/public/{*,.*} frontend/dist/site
