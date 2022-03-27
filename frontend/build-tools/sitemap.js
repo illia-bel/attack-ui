afb23b1e26ec83d5457c303855122f29126d0ccc
ilya-belik <illia.belichenko@gmail.com>
2022-03-27 12:23:31 +0300

    global sitemap

---
+++ frontend/build-tools/sitemap.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+
+const jsonSitemapPaths = ['frontend/app/public/sitemap.json', 'frontend/site/public/sitemap.json']
+const pagesList = []
+
+for (let i = 0; i < jsonSitemapPaths.length; i++) {
+  const pathJsonSitemap = jsonSitemapPaths[i]
+
+  try {
+    const readSitemap = JSON.parse(fs.readFileSync(pathJsonSitemap, 'utf8')).map(({ location, lastmod }) => {
+      return {
+        location,
+        lastmod: lastmod || new Date().toLocaleDateString().replace(/\./g, '-'),
+      }
+    })
+
+    pagesList.push(...readSitemap)
+  } catch (error) {
+    console.error(pathJsonSitemap, error)
+  }
+}
+
+const generateSitemapData = () => {
+  let urlSet = ``
+
+  pagesList.forEach(({ location, lastmod }) => {
+    urlSet =
+      urlSet +
+      `<url>
+  <loc>${location}</loc>
+  <lastmod>${lastmod}</lastmod>
+</url>`
+  })
+
+  const sitemapData = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${urlSet}
+</urlset>`
+
+  return sitemapData
+}
+
+const sitemap = generateSitemapData()
+fs.writeFileSync('frontend/dist/sitemap.xml', sitemap)
